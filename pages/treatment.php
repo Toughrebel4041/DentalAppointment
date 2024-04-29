96949810c741ce634cbcc7612c704582e36c8f22
suryaaa33 <dbangtan38@gmail.com>
2024-04-30 04:44:10 +0700

    perbaikan treatment page

---
+++ pages/treatment.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+  <title>Treatment SSR Clinic</title>
+  <style>
+
+  </style>
+</head>
+
+<body>
+  <header>
+    <?php
+
+    ?>
+  </header>
+</body>
+<div class="container m-4">
+        <div class="row row-cols-1 row-cols-md-2 
+					row-cols-lg-3 g-4">
+            <div class="col">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-6 text-center">
+                            <img src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card-body">
+                                <h5 class="card-title">
+                                    Data Structures
+                                </h5>
+                                <p class="card-text">
+                                    A data structure is a storage that is used to store and organize data.
+                                </p>
+                                <p class="card-text">
+                                    <small class="text-muted">
+                                        Last updated now
+                                    </small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-6 text-center">
+                            <img src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20190710102234/download3.png" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card-body">
+                                <h5 class="card-title">Algorithms</h5>
+                                <p class="card-text">
+                                    Therefore Algorithm refers to a sequence of finite steps to solve a particular problem.
+                                </p>
+                                <p class="card-text">
+                                    <small class="text-muted">
+                                        Last updated now
+                                    </small>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-6 text-center">
+                            <img src="https://cdn.hellosehat.com/wp-content/uploads/2019/01/Invisalign-Clarity-Braces-700x467.jpg" class="img-fluid rounded-start" alt="..." width="200" height="300">
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card-body">
+                            <h3><a href="#" class="text-pink">Behel Gigi (Ceramic)</a></h3>
+                                <p class="card-text">
+                                    ini behel gigi ceramic buat apa gitu-gitu
+                                </p>
+                                <a href="#" class="btn btn-primary">More Info</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+  
+</body>
+
+</html>
