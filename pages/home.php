89cdd77eadcb48b25108e02884741fbc8eaebb77
suryaaa33 <dbangtan38@gmail.com>
2024-06-09 03:27:07 +0700

    rapihin home dan treatment

--- pages/home.php
+++ pages/home.php
@@ -77,7 +77,7 @@
     </div>
     <div class="carousel-item">
       <img
-        src="https://img.freepik.com/premium-psd/dentist-dental-care-instagram-post-social-media-banner-square-flyer-poster-template-design_567310-120.jpg"
+        src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/dental-tumblr-banner-design-template-89c87525733b1f86b009ad8ab854471a_screen.jpg?ts=1633966272"
         class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
@@ -105,7 +105,7 @@
     <br>
     <h2 class="text-center mb-3">Treatment</h2>
     <div class="text-center mx-5">
-      <div class="row grid gap-3">
+      <div class="row grid justify-content-center gap-3">
         <div class="card col-sm-4" style="width: 27.86rem;">
           <h5 class="card-header">Treatment</h5>
           <div class="card-body">
@@ -115,7 +115,6 @@
               class="img-thumbnail" alt="...">
             <p class="card-text">Bleaching Gigi atau pemutihan gigi merupakan prosedur estetika yang digunakan untuk
               membuat permukaan gigi tampak lebih putih, dan bersih.</p>
-            <a href="#" class="btn btn-primary">More Info</a>
           </div>
         </div>
         <div class="card col-sm-4" style="width: 27.86rem;">
@@ -127,18 +126,6 @@
               class="img-thumbnail" alt="...">
             <p class="card-text">Direct veneer adalah jenis veneer yang dipasang secara langsung pada pasien menggunakan
               bahan resin komposit.</p>
-            <a href="#" class="btn btn-primary">More Info</a>
-          </div>
-        </div>
-        <div class="card col-sm-4" style="width: 27.86rem;">
-          <h5 class="card-header">Treatment</h5>
-          <div class="card-body">
-            <h5 class="card-title">Indirect veneer</h5>
-            <img src="https://d1ojs48v3n42tp.cloudfront.net/medical_procedures/58354_2-11-2021_18-18-34.jpeg"
-              class="img-thumbnail" alt="...">
-            <p class="card-text">Inderect veneer adalah jenis veneer yang proses pemasangan tidak
-              dilakukan langsung pada gigi.</p>
-            <a href="#" class="btn btn-primary">More Info</a>
           </div>
         </div>
         <div class="card col-sm-4" style="width: 27.86rem;">
@@ -149,8 +136,7 @@
               class="img-thumbnail" alt="...">
             <p class="card-text">Zirconia adalah salah satu bahan yang dapat digunakan untuk gigi palsu, bahan ini
               terbuat dari titanium.</p>
-            <a href="#" class="btn btn-primary">More Info</a>
-          </div>
+        </div>
         </div>
         <div class="card col-sm-4" style="width: 27.86rem;">
           <h5 class="card-header">Treatment</h5>
@@ -161,8 +147,6 @@
               class="img-thumbnail" alt="...">
             <p class="card-text">Gigi tiruan permanen bahan PFM adalah salah satu jenis gigi tiruan dengan bahan metal
               yang diindikasikan pada pasien yang kehilangan gigi aslinya, dan sangat disarankan untuk gigi belakang.
-            </p>
-            <a href="#" class="btn btn-primary">More Info</a>
           </div>
         </div>
         <div class="card col-sm-4" style="width: 27.86rem;">
@@ -174,7 +158,6 @@
               class="img-thumbnail" alt="...">
             <p class="card-text">Scaling Gigi adalah prosedur untuk melakukan pembersihan karang gigi. Karang gigi
               terbentuk dari tumpukan plak yang menempel pada permukaan gigi. </p>
-            <a href="#" class="btn btn-primary">More Info</a>
           </div>
         </div>
         <div class="card col-sm-4" style="width: 27.86rem;">
@@ -186,22 +169,9 @@
               class="img-thumbnail" alt="...">
             <p class="card-text">Kawat Gigi atau biasa disebut sebagai Behel adalah alat berbasis kawat yang digunakan
               oleh ortodontis untuk memperbaiki gigi atau rahang yang tidak rata dan gigi yang bertumpuk.</p>
-            <a href="#" class="btn btn-primary">More Info</a>
-          </div>
-        </div>
-        <div class="card col-sm-4" style="width: 27.86rem;">
-          <h5 class="card-header">Treatment</h5>
-          <div class="card-body">
-            <h5 class="card-title">Behel gigi (Ceramic)</h5>
-            <img src="https://cdn.hellosehat.com/wp-content/uploads/2019/01/Invisalign-Clarity-Braces-700x467.jpg"
-              class="img-thumbnail" alt="...">
-            <p class="card-text">Ortho dengan berbahan ceramic ini yang sewarna dengan gigi, sehingga tampilan behel
-              menyatu dengan warna gigi, cocok digunakan pada orang yang mementingkan estetik pada pekerjaaanya.</p>
-            <a href="#" class="btn btn-primary">More Info</a>
           </div>
         </div>
       </div>
-      <a href="index.php?p=treatment" class="btn btn-primary my-4">More Treatment</a>
     </div>
     <!-- Treatment end -->
 
@@ -216,7 +186,7 @@
               <img
                 src="https://media.licdn.com/dms/image/D5603AQEALmZwW2oc4A/profile-displayphoto-shrink_800_800/0/1692865198786?e=1720051200&v=beta&t=mk7gfUTHicXqUYK4mrxLFAwH01OcruNtF8CQtHKKxWE"
                 class="img-thumbnail" alt="...">
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum malesuada</p>
+              <p class="card-text">Drg. Raihan merupakan seorang spesialis tambal gigi yang mahir dalam menangani kerusakan gigi akibat karies</p>
               <a href="#" class="btn btn-primary">More Info</a>
             </div>
           </div>
@@ -226,7 +196,7 @@
               <img
                 src="https://media.licdn.com/dms/image/D5603AQFWCyFhP1TUZg/profile-displayphoto-shrink_400_400/0/1717168612779?e=1722470400&v=beta&t=tVTacKsp8b5RlhKxqSDRAcXj9tiUrM5mlcQArf9vKAo"
                 class="img-thumbnail" alt="...">
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum malesuada</p>
+              <p class="card-text">Drg. Suryandari dikenal karena pendekatannya yang ramah dan teliti dalam perawatan gigi berlubang.</p>
               <a href="#" class="btn btn-primary">More Info</a>
             </div>
           </div>
@@ -236,7 +206,7 @@
               <img
                 src="Foto Radi.jpg"
                 class="img-thumbnail" alt="...">
-              <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum malesuada</p>
+              <p class="card-text">Drg. Aiman berfokus pada pemulihan struktur gigi yang rusak dengan teknik modern dan bahan berkualitas tinggi</p>
               <a href="#" class="btn btn-primary">More Info</a>
             </div>
           </div>
@@ -246,35 +216,36 @@
 
     <!-- Testimoni -->
     <h2 class="text-center my-3">Testimoni</h2>
-    <div class="row row-cols-1 row-cols-md-3 g-4 mx-3 mb-3">
-      <div class="col">
-        <div class="card h-100">
-          <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum malesuada</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum malesuada</p>
-          </div>
-        </div>
-      </div>
-      <div class="col">
-        <div class="card h-100">
-          <img src="..." class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum malesuada</p>
-          </div>
-        </div>
+<div class="row row-cols-1 row-cols-md-3 g-4 mx-3 mb-3">
+  <div class="col">
+    <div class="card h-100">
+      <img src="http://klinikjoydental.com/wp-content/uploads/2014/05/tambal-gigi-depan-by-GJP.jpg" class="card-img-top img-fluid" alt="Tambal Gigi Patah">
+      <div class="card-body">
+        <h5 class="card-title">Tambal Gigi Patah</h5>
+        <p class="card-text">Gigi patah bisa diperbaiki kembali dengan rapih agar senyumanmu terbit kembali!</p>
       </div>
     </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://i.ytimg.com/vi/2NZKJb8cia4/maxresdefault.jpg" class="card-img-top img-fluid" alt="Scalling Gigi">
+      <div class="card-body">
+        <h5 class="card-title">Scaling Gigi</h5>
+        <p class="card-text">Yuk lakukan scaling gigi rutin setiap 6 bulan sekali, jangan biarkan kotoran menumpuk di gigimu!</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card h-100">
+      <img src="https://i.ytimg.com/vi/C1cRa9SBQGk/maxresdefault.jpg" class="card-img-top img-fluid" alt="Veneer Gigi">
+      <div class="card-body">
+        <h5 class="card-title">Veneer Gigi</h5>
+        <p class="card-text">Gigi kusam dan tidak pede? Veneer solusinya! <br> Kilaukan senyuman terbaikmu setiap hari berkat Veneer di SSR Dental Clinic!</p>
+      </div>
+    </div>
+  </div>
+</div>
+
     <!-- Testimoni end -->
   </div>
 </div>
