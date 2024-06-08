636d97be5469c7575d59091609dc74bb36daad65
andipermana444 <99400301+andipermana444@users.noreply.github.com>
2024-06-08 18:22:54 +0700

    Update home.php

--- pages/home.php
+++ pages/home.php
@@ -27,50 +27,67 @@
     background-color: #f8f9fa;
     padding: 3rem 0;
   }
-  img{
-    width:150px;
-    height:150px;
+
+  img {
+    width: 150px;
+    height: 150px;
   }
 
   .carousel-item img {
-  width: 100%;
-  height: auto;
-}
+    width: 100%;
+    height: auto;
+  }
   
 </style>
 <br>
 <br>
 <div>
-  <div>
+
+ 
     <!-- carousel -->
-    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/dentist-banner-design-template-5eac4c6f7642c93b594dd1abc980086a_screen.jpg?ts=1645015198" class="d-block w-100 img-fluid" alt="...">
+    <div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+        aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+        aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+        aria-label="Slide 3"></button>
     </div>
-    <div class="carousel-item">
-      <img src="https://img.freepik.com/premium-psd/dentist-dental-care-instagram-post-social-media-banner-square-flyer-poster-template-design_567310-120.jpg" class="d-block w-100 img-fluid" alt="...">
+    <div class="carousel-inner">
+      <div class="carousel-item active" >
+        <img
+          src="https://d1csarkz8obe9u.cloudfront.net/posterpreviews/dentist-banner-design-template-5eac4c6f7642c93b594dd1abc980086a_screen.jpg?ts=1645015198"
+          class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img
+          src="https://img.freepik.com/premium-psd/dentist-dental-care-instagram-post-social-media-banner-square-flyer-poster-template-design_567310-120.jpg"
+          class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://content.wepik.com/statics/15536894/preview-page0.jpg" class="d-block w-100" alt="...">
+      </div>
     </div>
-    <div class="carousel-item">
-      <img src="https://content.wepik.com/statics/15536894/preview-page0.jpg" class="d-block w-100 img-fluid" alt="...">
-    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden-focusable">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+      data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden-focusable">Next</span>
+    </button>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Previous</span>
-  </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="visually-hidden">Next</span>
-  </button>
-</div>
+  <div>
 
     <!-- carousel end -->
 
 
     <!-- Treatment -->
-     <br>
-     <br>
+    <br>
+    <br>
     <h2 class="text-center mb-3">Treatment</h2>
     <div class="text-center mx-5">
       <div class="row grid gap-3">
@@ -78,7 +95,9 @@
           <h5 class="card-header">Treatment</h5>
           <div class="card-body">
             <h5 class="card-title">Bleaching</h5>
-            <img src="https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2023/08/29043653/kategori-Bleaching-Gigi.jpg" class="img-thumbnail" alt="...">
+            <img
+              src="https://d1vbn70lmn1nqe.cloudfront.net/prod/wp-content/uploads/2023/08/29043653/kategori-Bleaching-Gigi.jpg"
+              class="img-thumbnail" alt="...">
             <p class="card-text">Bleaching Gigi atau pemutihan gigi merupakan prosedur estetika yang digunakan untuk
               membuat permukaan gigi tampak lebih putih, dan bersih.</p>
             <a href="#" class="btn btn-primary">More Info</a>
@@ -88,7 +107,9 @@
           <h5 class="card-header">Treatment</h5>
           <div class="card-body">
             <h5 class="card-title">Derect veneer</h5>
-            <img src="https://res.cloudinary.com/dk0z4ums3/image/upload/v1611295592/attached_image/veneer-gigi-ini-yang-harus-anda-ketahui.jpg" class="img-thumbnail" alt="...">
+            <img
+              src="https://res.cloudinary.com/dk0z4ums3/image/upload/v1611295592/attached_image/veneer-gigi-ini-yang-harus-anda-ketahui.jpg"
+              class="img-thumbnail" alt="...">
             <p class="card-text">Direct veneer adalah jenis veneer yang dipasang secara langsung pada pasien menggunakan
               bahan resin komposit.</p>
             <a href="#" class="btn btn-primary">More Info</a>
@@ -98,7 +119,8 @@
           <h5 class="card-header">Treatment</h5>
           <div class="card-body">
             <h5 class="card-title">Indirect veneer</h5>
-            <img src="https://d1ojs48v3n42tp.cloudfront.net/medical_procedures/58354_2-11-2021_18-18-34.jpeg" class="img-thumbnail" alt="...">
+            <img src="https://d1ojs48v3n42tp.cloudfront.net/medical_procedures/58354_2-11-2021_18-18-34.jpeg"
+              class="img-thumbnail" alt="...">
             <p class="card-text">Inderect veneer adalah jenis veneer yang proses pemasangan tidak
               dilakukan langsung pada gigi.</p>
             <a href="#" class="btn btn-primary">More Info</a>
@@ -108,7 +130,8 @@
           <h5 class="card-header">Treatment</h5>
           <div class="card-body">
             <h5 class="card-title">Gigi palsu (Zirconia)</h5>
-            <img src="https://arinidentalcare.com/wp-content/uploads/2024/02/kelebihan_gigi_tiruan_zirconia.jpg" class="img-thumbnail" alt="...">
+            <img src="https://arinidentalcare.com/wp-content/uploads/2024/02/kelebihan_gigi_tiruan_zirconia.jpg"
+              class="img-thumbnail" alt="...">
             <p class="card-text">Zirconia adalah salah satu bahan yang dapat digunakan untuk gigi palsu, bahan ini
               terbuat dari titanium.</p>
             <a href="#" class="btn btn-primary">More Info</a>
@@ -118,7 +141,9 @@
           <h5 class="card-header">Treatment</h5>
           <div class="card-body">
             <h5 class="card-title">Gigi palsu (PFM)</h5>
-            <img src="https://platinumdental.com.my/wp-content/uploads/elementor/thumbs/image8-pbhlm6sk5pea15vdn6doqkfhz47p6ilvith1klbe0o.jpg" class="img-thumbnail" alt="...">
+            <img
+              src="https://platinumdental.com.my/wp-content/uploads/elementor/thumbs/image8-pbhlm6sk5pea15vdn6doqkfhz47p6ilvith1klbe0o.jpg"
+              class="img-thumbnail" alt="...">
             <p class="card-text">Gigi tiruan permanen bahan PFM adalah salah satu jenis gigi tiruan dengan bahan metal
               yang diindikasikan pada pasien yang kehilangan gigi aslinya, dan sangat disarankan untuk gigi belakang.
             </p>
@@ -129,7 +154,9 @@
           <h5 class="card-header">Treatment</h5>
           <div class="card-body">
             <h5 class="card-title">Scaling</h5>
-            <img src="https://res.cloudinary.com/dk0z4ums3/image/upload/v1668053492/attached_image/ketahui-apa-itu-scaling-gigi.jpg" class="img-thumbnail" alt="...">
+            <img
+              src="https://res.cloudinary.com/dk0z4ums3/image/upload/v1668053492/attached_image/ketahui-apa-itu-scaling-gigi.jpg"
+              class="img-thumbnail" alt="...">
             <p class="card-text">Scaling Gigi adalah prosedur untuk melakukan pembersihan karang gigi. Karang gigi
               terbentuk dari tumpukan plak yang menempel pada permukaan gigi. </p>
             <a href="#" class="btn btn-primary">More Info</a>
@@ -139,7 +166,9 @@
           <h5 class="card-header">Treatment</h5>
           <div class="card-body">
             <h5 class="card-title">Behel gigi (Metal)</h5>
-            <img src="https://static.hdmall.id/system/image_attachments/images/000/041/332/original/shutterstock_417290032.jpg" class="img-thumbnail" alt="...">
+            <img
+              src="https://static.hdmall.id/system/image_attachments/images/000/041/332/original/shutterstock_417290032.jpg"
+              class="img-thumbnail" alt="...">
             <p class="card-text">Kawat Gigi atau biasa disebut sebagai Behel adalah alat berbasis kawat yang digunakan
               oleh ortodontis untuk memperbaiki gigi atau rahang yang tidak rata dan gigi yang bertumpuk.</p>
             <a href="#" class="btn btn-primary">More Info</a>
@@ -149,7 +178,8 @@
           <h5 class="card-header">Treatment</h5>
           <div class="card-body">
             <h5 class="card-title">Behel gigi (Ceramic)</h5>
-            <img src="https://cdn.hellosehat.com/wp-content/uploads/2019/01/Invisalign-Clarity-Braces-700x467.jpg" class="img-thumbnail" alt="...">
+            <img src="https://cdn.hellosehat.com/wp-content/uploads/2019/01/Invisalign-Clarity-Braces-700x467.jpg"
+              class="img-thumbnail" alt="...">
             <p class="card-text">Ortho dengan berbahan ceramic ini yang sewarna dengan gigi, sehingga tampilan behel
               menyatu dengan warna gigi, cocok digunakan pada orang yang mementingkan estetik pada pekerjaaanya.</p>
             <a href="#" class="btn btn-primary">More Info</a>
@@ -168,7 +198,9 @@
           <div class="card col-sm-3">
             <div class="card-body">
               <h5 class="card-title">Drg. Raihan Sultan Pasha Basuki</h5>
-              <img src="https://media.licdn.com/dms/image/D5603AQEALmZwW2oc4A/profile-displayphoto-shrink_800_800/0/1692865198786?e=1720051200&v=beta&t=mk7gfUTHicXqUYK4mrxLFAwH01OcruNtF8CQtHKKxWE" class="img-thumbnail" alt="...">
+              <img
+                src="https://media.licdn.com/dms/image/D5603AQEALmZwW2oc4A/profile-displayphoto-shrink_800_800/0/1692865198786?e=1720051200&v=beta&t=mk7gfUTHicXqUYK4mrxLFAwH01OcruNtF8CQtHKKxWE"
+                class="img-thumbnail" alt="...">
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum malesuada</p>
               <a href="#" class="btn btn-primary">More Info</a>
             </div>
@@ -176,7 +208,9 @@
           <div class="card col-sm-3">
             <div class="card-body">
               <h5 class="card-title">Drg. Suryandari Puspita Hartiati</h5>
-              <img src="https://scontent.fcgk4-3.fna.fbcdn.net/v/t39.30808-6/440268975_2848352585312393_56305696031118310_n.jpg?stp=c0.58.526.526a_dst-jpg_p526x296&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEhckPO5yEzV2jeSCKeoMSD3YDD_uFh7KjdgMP-4WHsqDOARAwsTB-J4a9qRK_4DJb7kVQVpnUR_LhzRZtNZa7E&_nc_ohc=Xc4wM7dafbAQ7kNvgH-r0rv&_nc_zt=23&_nc_ht=scontent.fcgk4-3.fna&oh=00_AYDriA2M0ihjGsDbQFU0L2iaSc9OIrb3TnNZcK0CF4RVNQ&oe=665D5B6F" class="img-thumbnail" alt="...">
+              <img
+                src="https://scontent.fcgk4-3.fna.fbcdn.net/v/t39.30808-6/440268975_2848352585312393_56305696031118310_n.jpg?stp=c0.58.526.526a_dst-jpg_p526x296&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEhckPO5yEzV2jeSCKeoMSD3YDD_uFh7KjdgMP-4WHsqDOARAwsTB-J4a9qRK_4DJb7kVQVpnUR_LhzRZtNZa7E&_nc_ohc=Xc4wM7dafbAQ7kNvgH-r0rv&_nc_zt=23&_nc_ht=scontent.fcgk4-3.fna&oh=00_AYDriA2M0ihjGsDbQFU0L2iaSc9OIrb3TnNZcK0CF4RVNQ&oe=665D5B6F"
+                class="img-thumbnail" alt="...">
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum malesuada</p>
               <a href="#" class="btn btn-primary">More Info</a>
             </div>
@@ -184,7 +218,9 @@
           <div class="card col-sm-3">
             <div class="card-body">
               <h5 class="card-title">Drg. Raditya Aiman Permana</h5>
-              <img src="https://scontent-cgk1-1.xx.fbcdn.net/v/t1.6435-9/118901432_2927133837573315_8644131355994684441_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGnI4HKXmoDG1pu5lXqaCuwCsqodp5J1LsKyqh2nknUu6qpDBvutZ_P-eK7xB_v2t4eiOB-UnM1FhPwSVFWbMPD&_nc_ohc=wCu8NvWWPIEQ7kNvgEdmO4C&_nc_ht=scontent-cgk1-1.xx&cb_e2o_trans=t&oh=00_AfDFv0GzdHAHN1nuyrswsp0G8qg5pA-fbFgKqzNJFZkGrA&oe=66573AEE" class="img-thumbnail" alt="...">
+              <img
+                src="https://scontent-cgk1-1.xx.fbcdn.net/v/t1.6435-9/118901432_2927133837573315_8644131355994684441_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGnI4HKXmoDG1pu5lXqaCuwCsqodp5J1LsKyqh2nknUu6qpDBvutZ_P-eK7xB_v2t4eiOB-UnM1FhPwSVFWbMPD&_nc_ohc=wCu8NvWWPIEQ7kNvgEdmO4C&_nc_ht=scontent-cgk1-1.xx&cb_e2o_trans=t&oh=00_AfDFv0GzdHAHN1nuyrswsp0G8qg5pA-fbFgKqzNJFZkGrA&oe=66573AEE"
+                class="img-thumbnail" alt="...">
               <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec bibendum malesuada</p>
               <a href="#" class="btn btn-primary">More Info</a>
             </div>
