e1e42fa298a9e05d9616757b1c59a95e8ed73e6a
Toughrebel4041 <raihan8pasha@gmail.com>
2024-06-14 17:59:11 +0700

    some changes on indexes

--- dentisthome.php
+++ dentisthome.php
@@ -8,7 +8,8 @@ require "inc.koneksi.php";
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <title>Index.php</title>
+  <link rel="shortcut icon" href="https://t4.ftcdn.net/jpg/03/02/68/11/360_F_302681154_9HOWdvGLtCKpfwO5B85yESszG7MfmlUl.jpg" type="image/x-icon">
+  <title>Dental Appointment</title>
 
   <style>
     header {
